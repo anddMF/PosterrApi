231e6c6dfaebbb120b62837694c3a9f43ca82d22
Andrew Moraes <andrewmoraesds@gmail.com>
2024-08-13 17:54:31 -0400

    add DAO

---
+++ Posterr.API/Infrastructure/DAL/DAO/PostDAO.cs
@@ -0,0 +1,24 @@
+﻿namespace Posterr.API.Infrastructure.DAL.DAO
+{
+    public class PostDAO
+    {
+        public int id { get; set; }
+        public int id_type { get; set; }
+        public string name { get; set; }
+        public int id_user { get; set; }
+        public string username { get; set; }
+        public int id_original_post { get; set; }
+        public string content { get; set; }
+        public DateTime post_date { get; set; }
+        public string second_content { get; set; }
+        public int second_type { get; set; }
+        public string second_username { get; set; }
+        public DateTime second_post_date { get; set; }
+        public int second_id_original { get; set; }
+        public string first_content { get; set; }
+        public int first_type { get; set; }
+        public string first_username { get; set; }
+        public DateTime first_post_date { get; set; }
+
+    }
+}
---
+++ Posterr.API/Infrastructure/DAL/DAO/UserDAO.cs
@@ -0,0 +1,11 @@
+﻿namespace Posterr.API.Infrastructure.DAL.DAO
+{
+    public class UserDAO
+    {
+        public int id { get; set; }
+        public string username { get; set; }
+        public int today_posts { get; set; }
+        public int total_posts { get; set; }
+        public DateTime registration_date { get; set; }
+    }
+}
