2eed12143062cf9d0fa74ba2265c2164959c941a
Andrew Moraes <andrewmoraesds@gmail.com>
2024-08-13 19:25:37 -0400

    add entities, services and controller

---
+++ Posterr.API/Services/PostService.cs
@@ -0,0 +1,76 @@
+﻿using Posterr.API.Entities;
+using Posterr.API.Infrastructure.DAL.DAO;
+using Posterr.API.Infrastructure.DAL;
+
+namespace Posterr.API.Services
+{
+    public class PostService
+    {
+        private DBCommunication _dbComm;
+
+        public PostService()
+        {
+            _dbComm = new DBCommunication(AppSettings.ConnectionStrings.MainDB);
+        }
+
+        public List<Post> GetPosts(PostQueryParameters queryParameters)
+        {
+            Dictionary<string, dynamic> param = ParamGetPosts(queryParameters.GetAll, queryParameters.PageNumber, queryParameters.PageSize, queryParameters.UserId, queryParameters.StartDate, queryParameters.EndDate);
+            List<PostDAO> postsDAO = _dbComm.ExecuteGet<PostDAO>("STP_GET_POSTS", param);
+
+            List<Post> response = TransformFromDAO(postsDAO);
+
+            return response;
+        }
+
+        // Assume that the frontend is validating the posts amount from the user
+        public void InsertPost(PostBody obj)
+        {
+            // validar posts do dia
+
+            if (obj.IdType > 1 && !obj.IdOriginalPost.HasValue)
+                throw new Exception("Posts that are not from type ORIGINAL require an IdOriginalPost");
+
+            Dictionary<string, dynamic> param = ParamInsertPost(obj);
+            _dbComm.ExecuteOperation("STP_INSERT_POST", param);
+        }
+
+        private Dictionary<string, dynamic> ParamGetPosts(bool getAll, int pageNumber, int pageSize, int? userId, DateTime startDate, DateTime? endDate)
+        {
+            return new Dictionary<string, dynamic>
+            {
+                { "page_number", pageNumber },
+                { "page_size", pageSize },
+                { "user_id", getAll ? null : userId },
+                { "start_date", startDate },
+                { "end_date", endDate }
+            };
+        }
+
+        private Dictionary<string, dynamic> ParamInsertPost(PostBody obj)
+        {
+            return new Dictionary<string, dynamic>
+            {
+                { "pid_type", obj.IdType },
+                { "pid_user", obj.IdUser },
+                { "pid_original_post", obj.IdOriginalPost },
+                { "pcontent", obj.Content },
+                { "ppost_date", obj.PostDate }
+            };
+        }
+
+        private List<Post> TransformFromDAO(List<PostDAO> daoList)
+        {
+            List<Post> response = new List<Post>();
+
+            for (int i = 0; i < daoList.Count; i++)
+            {
+                PostDAO current = daoList[i];
+                Post converted = new Post(current);
+                response.Add(converted);
+            }
+
+            return response;
+        }
+    }
+}
---
+++ Posterr.API/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using Posterr.API.Entities;
+using Posterr.API.Infrastructure.DAL.DAO;
+using Posterr.API.Infrastructure.DAL;
+
+namespace Posterr.API.Services
+{
+    public class UserService
+    {
+        private DBCommunication _dbComm;
+
+        public UserService()
+        {
+            _dbComm = new DBCommunication(AppSettings.ConnectionStrings.MainDB);
+        }
+
+        public User GetUser(int userId)
+        {
+            var param = new Dictionary<string, object>() { { "user_id", userId } };
+            List<UserDAO> usersDAO = _dbComm.ExecuteGet<UserDAO>("STP_GET_USER", param);
+
+            return new User(usersDAO.First());
+        }
+
+        public int GetTodayPostsUsage(int userId)
+        {
+            User user = GetUser(userId);
+
+            return user.TodayPosts;
+        }
+    }
+}
