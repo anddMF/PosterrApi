a740ed5f58c4c5ef7c8c11df4e13a3dcd9c28d6f
Andrew Moraes <andrewmoraesds@gmail.com>
2024-08-13 17:22:56 -0400

    add error handler and settings as singleton

---
+++ Posterr.API/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Serilog;
+using System.Net;
+
+namespace Posterr.API.Middlewares
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleException(context, ex);
+            }
+        }
+
+        private static Task HandleException(HttpContext context, Exception exception)
+        {
+            Log.Error(exception, "Error");
+
+            var code = HttpStatusCode.InternalServerError;
+
+            var result = System.Text.Json.JsonSerializer.Serialize(new { errors = new { Message = exception?.Message } });
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
